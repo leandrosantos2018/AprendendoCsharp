ab785a49723b7a1ccd3d6049444dde2cdd204145
Administrador <Administrador@DESKTOP-J8CUCBV>
2022-12-01 20:45:10 -0300

    Adicionar arquivos de projeto.

---
+++ 1-OlaMundo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Programa
+{
+
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Ola, mundo");
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
+
+
---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variaveis Poonto Flutuante");
+        double salario;
+
+        salario = 3000.13;
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle enter para fechar...");
+
+        Console.ReadLine();
+
+
+    }
+}
