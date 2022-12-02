5ba11dbd25d2e57567f1f5da634e5822448af4ce
Leandro Santos <leandrolrs2021@outlook.com>
2022-12-02 07:38:35 -0300

    você aprendeu como: Usar o if, que irá permitir que nosso código execute determinados comandos de acordo com uma condição pré estabelecida; Usar as operações lógicas AND (&&) e OR (||), operadores lógicos, que são usados quando precisamos realizar operações sobre um ou dois valores booleano; Trabalhar com o escopo de variáveis, entendendo como é sua visibilidade dentro do programa e em que partes elas podem ser utilizadas.

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 7 - Condicionais");
+
+        int idade = 16;
+        bool acompanhandoDosPais = false;
+
+        if (idade >= 18 || acompanhandoDosPais)
+        {
+            Console.WriteLine("Pode entrar");
+        }
+        else
+        {
+            Console.WriteLine("NNão pode entrar");
+        }
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
