dce3b7111daecd55b4e819bf7ed955d454217013
Leandro Santos <leandrolrs2021@outlook.com>
2022-12-02 06:19:53 -0300

    Nessa aula, você aprendi: O conceito e como declarar char e String, e permitir que seja possível armazenar nomes ou frases; A concatenar Strings, e permitir a junção de mais de uma variável desse tipo; Que variáveis guardam valores e não referências.

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando p Projero 5 - Caractres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Alura -  Cursos de Tecnologia ";
+
+        Console.WriteLine(primeiraFrase + 2022);
+
+        string[] curso = { "Javascript", "C#", "PHP" };
+
+        Console.WriteLine(curso[1]);
+
+        string curso2 = @"Curso:" +
+" - Go " +
+"- C# " +
+"- Python";
+        
+
+        Console.WriteLine(curso2 );
+
+        Console.WriteLine("Tecle enter para fechar...");
+
+        Console.ReadLine();
+
+
+    }
+}
